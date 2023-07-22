5599a55c007da76717b9a8c4e48871f528d61294
moe1129peterson <moe1129tsuzuki@gmail.com>
2023-07-22 17:35:17 -0600

    Connected to openai API

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-import { Configuration, OpenAIApi } from "openai";
+const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
   organization: "org-eMWqhAYA1nltzU9N5TTJt2IG",
-  apiKey: "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",
+  apiKey: "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",
 });
 const openai = new OpenAIApi(configuration);
-// const response = await openai.listEngines();
+async function callApi() {
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: "Say this is a test",
+    max_tokens: 7,
+    temperature: 0,
+  });
+  console.log(response.data.choices[0].text);
+}
+
+callApi();
+
+//create a simple
